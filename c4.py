3466d9d41535f8f29b5790767d087e39d952ed3e
0x0a λ <74163590+OxOa2@users.noreply.github.com>
2020-11-11 15:52:06 -0300

    up

--- c4.py
+++ c4.py
@@ -2,14 +2,41 @@ import requests as reqs
 from time import sleep
 import os
 
+N = '\x1b[0m'
+D = '\x1b[90m'
+W = '\x1b[1;37m'
+B = '\x1b[1;34m'
+R = '\x1b[1;31m'
+G = '\x1b[1;32m'
+Y = '\x1b[1;33m'
+C = '\x1b[1;36m'
+wd = '\x1b[90;1m'
+GL = '\x1b[96;1m'
+BB = '\x1b[34;1m'
+GG = '\x1b[32;1m'
+RR = '\x1b[31;1m'
+B = '\x1b[34m'
+G = '\x1b[32m'
+R = '\x1b[31m'
+ask = G + '[' + W + '?' + G + '] '
+sukses = G + '[' + W + '\xe2\x88\x9a' + G + '] '
+eror = R + '[' + W + '!' + R + ']'
+
+def runntxt(s):
+    for noobs in s + '\n':
+        sys.stdout.write(noobs)
+        sys.stdout.flush()
+        time.sleep(10 / 2100)
+
+
 def main():
-    IP = input('Digite o ip do provedor a ser consultado: ')
-    print('033[94;1mConsultando IP do provedor...')
+    IP = input(GL'Digite o ip do provedor a ser consultado: ')
+    print(GL'Consultando IP do provedor...')
 
 
 def banner():
     os.system("clear")
-    print("Carregando...")
+    print(WD"Carregando...")
     sleep(2)
     print("""________ __  _   ______________
   / ____/ // / / | / / ____/_  __/
@@ -26,21 +53,21 @@ banner()
 try:
     IP = input('Digite o ip do provedor a ser consultado: ')
 except valueError:
-    print('033[94;1mUtilize somente numeros em ip. não consultamos mac addres')
+    print(BB'Utilize somente numeros em ip. não consultamos mac addres')
 
 try:
     r = reqs.get('http://ip-api.com/json/'+IP).json()
     ip = r['query']
     pais = r['country']
 except valueError:
-    print('Para utilizar esse script você precisa instalar os requests do pip utize, pip install requests')
+    print(RR'Para utilizar esse script você precisa instalar os requests do pip utize, pip install requests')
 try:
     File "/data/data/com.termux/files/home/C4NET/c4.py", line 26, in <module>
     IP = input('Digite o ip do provedor a ser consultado: ')
 KeyboardInterrupt
 
 except valueError:
-    print('Você escolheu sair. obrigado pela preferencia')
+    print(wd'Você escolheu sair. obrigado pela preferencia')
 
 while True:
     r = reqs.get('http://ip-api.com/json/'+IP).json()
