43f15cd16aa69a3ed83e2ddd84271c0e4370a257
0x0a λ <74163590+OxOa2@users.noreply.github.com>
2020-11-11 02:53:18 -0300

    update c4.py

--- c4.py
+++ c4.py
@@ -1,7 +1,30 @@
 import requests as reqs
 def main():
     IP=input('Digite o ip do provedor a ser consultado: ')
-    print('Consultando IP do provedor...')
+    print('033[94;1mConsultando IP do provedor...')
+
+
+def banner():
+    os.system("clear")
+    print("033[91;1mCarregando =========")
+    time.sleep(2)
+    print("""________ __  _   ______________
+  / ____/ // / / | / / ____/_  __/
+ / /   / // /_/  |/ / __/   / /
+/ /___/__  __/ /|  / /___  / /
+\____/
+""")
+    os.system(
+"toilet -f sbmloc --filter -borde r;metal ' C4NET")
+banner()
+
+try:
+    IP=input('Digite o ip do provedor a ser consultado: ')
+    except valueError:
+        print('033[94;1mUtilize somente numeros em ip. não consultamos mac addres')
+
+for i range(1)
+    print('033[94;1mOcorreu um erro inesperado. tente instalar o request do pip. com pip install request')
 while True:
     r=reqs.get('http://ip-api.com/json/'+IP).json()
     ip=r['query']
