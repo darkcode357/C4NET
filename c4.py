9d057b8a6885d72c0507c876956809b2a208eaf5
0x0a λ <74163590+OxOa2@users.noreply.github.com>
2020-11-11 00:16:47 -0300

    Update c4.py

--- c4.py
+++ c4.py
@@ -1,12 +1,14 @@
 import requests as reqs
-IP=input('Digite o IP a ser consultado: ')
-print('Consultando IP:')
-r=reqs.get('http://ip-api.com/json/'+IP).json()
-ip=r['query']
-pais=r['country']
-provedor=r['isp']
-print(f'''
-IP: {ip}
-Pais: {pais}
-Provedor {provedor}
+def main():
+    IP=input('Digite o IP a ser consultado: ')
+    print('Consultando IP:')
+while true:
+    r=reqs.get('http://ip-api.com/json/'+IP).json()
+    ip=r['query']
+    pais=r['country']
+    provedor=r['isp']
+    print(f'''
+    IP: {ip}
+    Pais: {pais}
+    Provedor {provedor}
 ''')
